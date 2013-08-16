1e9798954b3339ca415f75fca3484b12c624aeb5
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-08-16 20:55:51 +0900

    xml 로 설정하니까 잘~ 된다....

---
+++ chapter10/src/test/java/kr/spring/batch/chapter10/test/ExternalizedFlowStepTest.java
@@ -0,0 +1,37 @@
+package kr.spring.batch.chapter10.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * kr.spring.batch.chapter10.test.ExternalizedFlowStepTest
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 13. 8. 16. 오후 8:52
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:kr/spring/batch/chapter10/batch-infrastructure.xml",
+                        "classpath:kr/spring/batch/chapter10/externalized-flow-step.xml" })
+public class ExternalizedFlowStepTest extends AbstractJobTest {
+
+    @Override
+    protected int getExpectedNbOfInvocationFileExists() {
+        return 2;
+    }
+
+    @Override
+    protected int getExpectedNbStepExecutionsDownloadedFileOkNoSkippedItems() {
+        return super.getExpectedNbStepExecutionsDownloadedFileOkNoSkippedItems() + 1;
+    }
+
+    @Override
+    protected int getExpectedNbStepExecutionsDownloadedFileOkSkippedItems() {
+        return super.getExpectedNbStepExecutionsDownloadedFileOkSkippedItems() + 1;
+    }
+
+    @Override
+    protected int getExpectedNbStepExecutionsNoDownloadedFile() {
+        return super.getExpectedNbStepExecutionsNoDownloadedFile() + 1;
+    }
+}
