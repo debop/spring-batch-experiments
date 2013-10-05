28b56381b43d15332cce3d2d5d907c9b6eee1e6d
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-10-05 17:33:03 +0900

    step 이름 수정.

--- chapter05/src/test/java/kr/spring/batch/chapter05/test/jpa/JobStructureJpaConfig.java
+++ chapter05/src/test/java/kr/spring/batch/chapter05/test/jpa/JobStructureJpaConfig.java
@@ -32,33 +32,33 @@ import javax.persistence.EntityManagerFactory;
 // @ImportResource({ "classpath:kr/spring/batch/chapter05/test/jpa/jobstructure-jpa.xml" })
 public class JobStructureJpaConfig extends AbstractBatchConfiguration {
 
-	@Autowired EntityManagerFactory emf;
+    @Autowired EntityManagerFactory emf;
 
-	@Bean
-	public Job importProductsJob() throws Exception {
+    @Bean
+    public Job importProductsJob() throws Exception {
 
-		Step step = stepBuilders.get("importProductsJob")
-		                        .<Product, Product>chunk(100)
-		                        .reader(productItemReader())
-		                        .writer(productItemWriter())
-		                        .build();
+        Step step = stepBuilders.get("importProductsStep")
+                                .<Product, Product>chunk(100)
+                                .reader(productItemReader())
+                                .writer(productItemWriter())
+                                .build();
 
-		return jobBuilders.get("importProductsJob").start(step).build();
-	}
+        return jobBuilders.get("importProductsJob").start(step).build();
+    }
 
-	@Bean
-	public ItemReader<Product> productItemReader() throws Exception {
-		JpaPagingItemReader<Product> reader = new JpaPagingItemReader<Product>();
-		reader.setEntityManagerFactory(emf);
-		reader.setQueryString("select p from Product p");
-		reader.setPageSize(5);
-		reader.afterPropertiesSet();
+    @Bean
+    public ItemReader<Product> productItemReader() throws Exception {
+        JpaPagingItemReader<Product> reader = new JpaPagingItemReader<Product>();
+        reader.setEntityManagerFactory(emf);
+        reader.setQueryString("select p from Product p");
+        reader.setPageSize(5);
+        reader.afterPropertiesSet();
 
-		return reader;
-	}
+        return reader;
+    }
 
-	@Bean
-	public ItemWriter<Product> productItemWriter() {
-		return new DummyProductItemWriter();
-	}
+    @Bean
+    public ItemWriter<Product> productItemWriter() {
+        return new DummyProductItemWriter();
+    }
 }
